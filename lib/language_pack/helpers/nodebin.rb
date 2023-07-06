f797ccec2285f29841ab50c0ff255d7ef13db4a1
Darren Rush <dlrush@gmail.com>
2023-07-06 10:18:18 -0700

    nodejs -> 20.4

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -2,10 +2,10 @@ require 'json'
 
 class LanguagePack::Helpers::Nodebin
   def self.hardcoded_node_lts
-    version = "16.18.1"
+    version = "20.4.0"
     {
       "number" => version,
-      "url"    => "https://heroku-nodebin.s3.us-east-1.amazonaws.com/node/release/linux-x64/node-v#{version}-linux-x64.tar.gz"
+      "url"    => "https://nodejs.org/download/release/latest-v20.x/node-v#{version}-linux-x64.tar.gz"
     }
   end
 
